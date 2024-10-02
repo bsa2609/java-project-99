9800c6e9f32b96b8158f6e25bd5474898e310b30
Sergey Breusov <bsa2609@yandex.ru>
2024-10-02 04:59:56 +0300

    init the spring boot project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-### Hexlet tests and linter status:
+# Task Manager
+
 [![Actions Status](https://github.com/bsa2609/java-project-99/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/bsa2609/java-project-99/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/bea3912e2efbf71976df/maintainability)](https://codeclimate.com/github/bsa2609/java-project-99/maintainability)
+[![Maintainability](https://api.codeclimate.com/v1/badges/bea3912e2efbf71976df/maintainability)](https://codeclimate.com/github/bsa2609/java-project-99/maintainability)
+[![Actions Status](https://github.com/bsa2609/java-project-99/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/bsa2609/java-project-99/actions)
+
+### Description:
+Task management system
---
+++ app/build.gradle.kts
@@ -0,0 +1,48 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+	application
+	checkstyle
+	jacoco
+	id("org.springframework.boot") version "3.3.4"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "hexlet.code"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+	testLogging {
+		exceptionFormat = TestExceptionFormat.FULL
+		events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+		// showStackTraces = true
+		// showCauses = true
+		showStandardStreams = true
+	}
+}
+
+tasks.jacocoTestReport {
+	reports {
+		xml.required.set(true)
+	}
+}
